e101b0bab94547fa9501678eccc19091c44c4db7
eastarj <eastarj@gmail.com>
2019-07-30 00:18:26 +0900

    debug code move

--- base/src/debug/java/eastar/base/BDApplication.kt
+++ base/src/debug/java/eastar/base/BDApplication.kt
@@ -1,8 +1,11 @@
 package eastar.base
 
+import android.app.NotificationManager
 import android.base.CApplication
 import android.content.Context
 import android.log.logActivity
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
 
 abstract class BDApplication : CApplication() {
 
@@ -15,5 +18,28 @@ abstract class BDApplication : CApplication() {
         super.onCreate()
         DD.onCreate(applicationContext)
         logActivity()
+        showDebugDBAddressLogToast(applicationContext)
+    }
+
+    private fun showDebugDBAddressLogToast(context: Context) {
+        try {
+            val debugDB = Class.forName("com.amitshekhar.DebugDB")
+            val getAddressLog = debugDB.getMethod("getAddressLog")
+            val value = getAddressLog.invoke(null) as String
+
+            val pm = context.packageManager
+            val applicationInfo = pm.getApplicationInfo(context.packageName, 0)
+            val name = pm.getApplicationLabel(applicationInfo).toString()
+
+            val notificationBuilder = NotificationCompat.Builder(context, "COMMON")
+                    .setSmallIcon(android.R.drawable.ic_notification_overlay)
+                    .setAutoCancel(false)
+                    .setContentTitle(name)
+                    .setContentText(value)
+            val nm = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            nm.notify(0 /* ID of notification */, notificationBuilder.build())
+            Toast.makeText(context, value, Toast.LENGTH_LONG).show()
+        } catch (ignore: Exception) {
+        }
     }
 }
