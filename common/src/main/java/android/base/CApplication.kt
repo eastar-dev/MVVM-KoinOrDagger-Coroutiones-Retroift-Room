a1c0529781058188ff789dc5c0662397683fda25
pluto <pluto@eastandroid.com>
2020-01-08 17:35:27 +0900

    실행하면 죽지않음

--- common/src/main/java/android/base/CApplication.kt
+++ common/src/main/java/android/base/CApplication.kt
@@ -1,4 +1,4 @@
 package android.base
 
-abstract class CApplication : dagger.android.DaggerApplication()
+abstract class CApplication : dagger.android.support.DaggerApplication()
 //abstract class CApplication : android.app.Application()
