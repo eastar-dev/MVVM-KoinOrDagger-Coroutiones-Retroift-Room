ef92e59c4bf2950a08706fbec7acd74ca3438e68
pluto <pluto@eastandroid.com>
2020-01-08 17:46:10 +0900

    다른거

--- app/src/main/java/dev/eastar/branch/app/AppActivity.kt
+++ app/src/main/java/dev/eastar/branch/app/AppActivity.kt
@@ -1,11 +1,14 @@
 package dev.eastar.branch.app
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_app.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +18,24 @@ class AppActivity : AppCompatActivity() {
         tv.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
             override fun onGlobalLayout() {
                 tv.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                val name = "1234567890"
+                run()
+            }
+        })
+
+    }
+
+    fun run() {
+        CoroutineScope(Dispatchers.Main).launch {
+            repeat(20) {
+                val tv = appCompatButton4
+                val name = "1234567890".take(it % 10)
                 val age = " , 20"
                 val avail: Float = tv.width.toFloat() - tv.paint.measureText(age)
                 val result = TextUtils.ellipsize(name, tv.paint, avail, TextUtils.TruncateAt.END)
                 tv.text = "$result$age"
+                orgText.text = "$name$age"
+                delay(1000)
             }
-        })
+        }
     }
 }
--- app/src/main/java/dev/eastar/branch/app/AppApplication.kt
+++ app/src/main/java/dev/eastar/branch/app/AppApplication.kt
@@ -1,9 +1,9 @@
 package dev.eastar.branch.app
 
 import dagger.android.AndroidInjector
-import dagger.android.DaggerApplication
+import dagger.android.support.DaggerApplication
 import eastar.base.BApplication
 
 class AppApplication : BApplication() {
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> = DaggerApplicationComponent.builder().application(this).context(this).build()
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication>? = DaggerApplicationComponent.builder().application(this).context(this).build()
 }
