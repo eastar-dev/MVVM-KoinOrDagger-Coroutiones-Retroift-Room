6adf3f26750b024781e62b30f0e883d58641161b
eastarj <eastarj@gmail.com>
2020-01-10 01:56:42 +0900

    no message

--- app/src/main/java/dev/eastar/branch/app/AppMain.kt
+++ app/src/main/java/dev/eastar/branch/app/AppMain.kt
@@ -1,16 +1,9 @@
 package dev.eastar.branch.app
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import dagger.android.support.DaggerAppCompatActivity
-//import dev.eastar.app_lib1.Lib1Activity
-//import dev.eastar.app_lib2.Lib2Activity
-
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import dev.eastar.branch.ui.BranchMain
-import javax.inject.Inject
 
 class AppMain : AppCompatActivity() {
 
