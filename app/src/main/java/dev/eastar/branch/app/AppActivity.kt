7c8673ed84c3bad55c3e6739a06e425fc017fe2b
pluto <pluto@eastandroid.com>
2020-07-16 17:51:19 +0900

    gradle update
    build bugbix

--- app/src/main/java/dev/eastar/branch/app/AppActivity.kt
+++ app/src/main/java/dev/eastar/branch/app/AppActivity.kt
@@ -1,40 +1,12 @@
 package dev.eastar.branch.app
 
 import android.os.Bundle
-import android.text.TextUtils
-import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_app.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import dev.eastar.mvvm.R
 
 class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app)
-        val tv = appCompatButton4
-        tv.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                tv.viewTreeObserver.removeOnGlobalLayoutListener(this)
-                run()
-            }
-        })
-    }
-
-    fun run() {
-        CoroutineScope(Dispatchers.Main).launch {
-            repeat(20) {
-                val tv = appCompatButton4
-                val name = "1234567890".take(it % 10)
-                val age = " , 20"
-                val avail: Float = tv.width.toFloat() - tv.paint.measureText(age)
-                val result = TextUtils.ellipsize(name, tv.paint, avail, TextUtils.TruncateAt.END)
-                tv.text = "$result$age"
-                orgText.text = "$name$age"
-                delay(1000)
-            }
-        }
     }
 }
